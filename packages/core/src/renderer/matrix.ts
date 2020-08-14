5d86b79a826769c69f4907473d37f26b401cb21e
spaguette <ar-rapoport@yandex.ru>
2020-08-14 15:51:34 +0200

    Actually filled in all finder patterns in matrix

--- packages/core/src/renderer/matrix.ts
+++ packages/core/src/renderer/matrix.ts
@@ -1,13 +1,25 @@
 import { EncodingResult } from './types'
 
+type Matrix = number[][]
+
 const FINDER_PATTERN_OUTER_SIZE = 7
+const FINDER_PATTERN_INNER_WHITE_SIZE = 5
+const FINDER_PATTERN_INNER_BLACK_SIZE = 3
 const PIXEL = {
   WHITE: 0,
   BLACK: 1,
 }
 
+const buildFinderPattern = (): Matrix => {
+  const edgeRow = Array(FINDER_PATTERN_OUTER_SIZE).fill(PIXEL.BLACK)
+  const secondToEdgeRow = [PIXEL.BLACK,  ...Array(FINDER_PATTERN_INNER_WHITE_SIZE).fill(PIXEL.WHITE), PIXEL.BLACK]
+  const innerRows = Array(FINDER_PATTERN_INNER_BLACK_SIZE).fill([ PIXEL.BLACK, PIXEL.WHITE, ...Array(FINDER_PATTERN_INNER_BLACK_SIZE).fill(PIXEL.BLACK), PIXEL.WHITE, PIXEL.BLACK ])
+
+  return [edgeRow, secondToEdgeRow, ...innerRows, secondToEdgeRow, edgeRow]
+}
+
 type Coordinates<X extends number = number, Y extends number = number> = [X, Y]
-type Matrix = number[][]
+
 
 interface FinderPatternCoordinates {
   TOP_LEFT: Coordinates<0, 0>
@@ -29,12 +41,17 @@ const calculateFinderPatternCoordinates = (
 export const generateMatrix = (encodingResult: EncodingResult): Matrix => {
   const qrCodeSize = calculateQrCodeSize(encodingResult)
   const finderPatternCoordinates = calculateFinderPatternCoordinates(qrCodeSize)
+  const finderPatternModules = buildFinderPattern()
 
   const matrix: Matrix = Array(qrCodeSize).fill([])
 
-  matrix[finderPatternCoordinates.TOP_LEFT[0]].push(
-    ...Array(FINDER_PATTERN_OUTER_SIZE).fill(PIXEL.BLACK)
-  )
+  for (const coordinates of Object.values(finderPatternCoordinates)) {
+    for (let x = 0; x < FINDER_PATTERN_OUTER_SIZE; x++) {
+      for (let y = 0; y < FINDER_PATTERN_OUTER_SIZE; y++) {
+        matrix[coordinates[0] + x][coordinates[1] + y] = finderPatternModules[x][y]
+      }
+    }
+  }
 
   return matrix
 }
