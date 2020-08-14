33260cf2e894ce630a222a39fd4159180c20bed7
spaguette <ar-rapoport@yandex.ru>
2020-08-14 16:59:10 +0200

    Fixed creation of 2D matrix

--- packages/core/src/renderer/matrix.ts
+++ packages/core/src/renderer/matrix.ts
@@ -3,21 +3,26 @@ import {
   calculateFinderPatternCoordinates,
   FINDER_PATTERN_OUTER_SIZE,
   FINDER_PATTERN,
-} from 'renderer/finderPattern'
+} from './finderPattern'
 
 const calculateQrCodeSize = ({ version }: EncodingResult): number =>
   (version - 1) * 4 + 21
+
+const build2DArray = (size: number) =>
+  Array(size)
+    .fill(null)
+    .map(() => Array(size).fill(null))
 
 export const generateMatrix = (encodingResult: EncodingResult): Matrix => {
   const qrCodeSize = calculateQrCodeSize(encodingResult)
   const finderPatternCoordinates = calculateFinderPatternCoordinates(qrCodeSize)
 
-  const matrix: Matrix = Array(qrCodeSize).fill([])
+  const matrix: Matrix = build2DArray(qrCodeSize)
 
   for (const coordinates of Object.values(finderPatternCoordinates)) {
     for (let x = 0; x < FINDER_PATTERN_OUTER_SIZE; x++) {
       for (let y = 0; y < FINDER_PATTERN_OUTER_SIZE; y++) {
-        matrix[coordinates[0] + x][coordinates[1] + y] = FINDER_PATTERN[x][y]
+        matrix[coordinates[1] + y][coordinates[0] + x] = FINDER_PATTERN[y][x]
       }
     }
   }
