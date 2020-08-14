9e407ea56fe8e4dad5c5c683d84ad50f5f2808be
spaguette <ar-rapoport@yandex.ru>
2020-08-14 15:26:17 +0200

    Started working on actually filling the matrix

--- packages/core/src/renderer/matrix.ts
+++ packages/core/src/renderer/matrix.ts
@@ -1,13 +1,18 @@
 import { EncodingResult } from './types'
 
 const FINDER_PATTERN_OUTER_SIZE = 7
+const PIXEL = {
+  WHITE: 0,
+  BLACK: 1,
+}
 
 type Coordinates<X extends number = number, Y extends number = number> = [X, Y]
+type Matrix = number[][]
 
 interface FinderPatternCoordinates {
   TOP_LEFT: Coordinates<0, 0>
   TOP_RIGHT: Coordinates<number, 0>
-  BOTTOM_LEFT: Coordinates<0, number>
+  BOTTOM_LEFT: Coordinates<0>
 }
 
 const calculateQrCodeSize = ({ version }: EncodingResult): number =>
@@ -21,9 +26,15 @@ const calculateFinderPatternCoordinates = (
   BOTTOM_LEFT: [0, qrCodeSize - FINDER_PATTERN_OUTER_SIZE],
 })
 
-export const generateMatrix = (encodingResult: EncodingResult): number[][] => {
+export const generateMatrix = (encodingResult: EncodingResult): Matrix => {
   const qrCodeSize = calculateQrCodeSize(encodingResult)
   const finderPatternCoordinates = calculateFinderPatternCoordinates(qrCodeSize)
 
-  return [[]]
+  const matrix: Matrix = Array(qrCodeSize).fill([])
+
+  matrix[finderPatternCoordinates.TOP_LEFT[0]].push(
+    ...Array(FINDER_PATTERN_OUTER_SIZE).fill(PIXEL.BLACK)
+  )
+
+  return matrix
 }
