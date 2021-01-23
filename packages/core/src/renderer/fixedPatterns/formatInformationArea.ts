d7a2eccaa220439c2f870d412eec2e1a3a1bfaf4
Yurick <yurick.hauschild@gmail.com>
2021-01-23 17:44:26 +0100

    fixup formatinformationarea

--- packages/core/src/renderer/fixedPatterns/formatInformationArea.ts
+++ packages/core/src/renderer/fixedPatterns/formatInformationArea.ts
@@ -6,26 +6,26 @@ import { FINDER_PATTERN_OUTER_SIZE } from './finder'
 const getInformationAreaCoordinates = (qrCodeSize: number) => {
   return [
     {
-      x: 0,
-      y: FINDER_PATTERN_OUTER_SIZE + 1,
+      x: FINDER_PATTERN_OUTER_SIZE + 1,
+      y: 0,
       width: 1,
       height: FINDER_PATTERN_OUTER_SIZE + 2,
     },
     {
-      x: FINDER_PATTERN_OUTER_SIZE + 1,
-      y: 0,
+      x: 0,
+      y: FINDER_PATTERN_OUTER_SIZE + 1,
       width: FINDER_PATTERN_OUTER_SIZE + 2,
-      height: 1,
-    },
-    {
-      x: FINDER_PATTERN_OUTER_SIZE + 1,
-      y: qrCodeSize - FINDER_PATTERN_OUTER_SIZE - 1,
-      width: FINDER_PATTERN_OUTER_SIZE + 1,
       height: 1,
     },
     {
       x: qrCodeSize - FINDER_PATTERN_OUTER_SIZE - 1,
       y: FINDER_PATTERN_OUTER_SIZE + 1,
+      width: FINDER_PATTERN_OUTER_SIZE + 1,
+      height: 1,
+    },
+    {
+      x: FINDER_PATTERN_OUTER_SIZE + 1,
+      y: qrCodeSize - FINDER_PATTERN_OUTER_SIZE - 1,
       width: 1,
       height: FINDER_PATTERN_OUTER_SIZE + 1,
     },
@@ -40,10 +40,10 @@ export const addFormatInformationAreaPattern = (
   matrix[4 * version + 9][8] = Pixel.BLACK
 
   for (const coordinate of getInformationAreaCoordinates(matrix.length)) {
-    for (const x of range(coordinate.x, coordinate.height)) {
-      for (const y of range(coordinate.y, coordinate.width)) {
-        if (matrix[x][y] === Pixel.EMPTY) {
-          matrix[x][y] = Pixel.WHITE
+    for (const x of range(coordinate.x, coordinate.width)) {
+      for (const y of range(coordinate.y, coordinate.height)) {
+        if (matrix[y][x] === Pixel.EMPTY) {
+          matrix[y][x] = Pixel.WHITE
         }
       }
     }
