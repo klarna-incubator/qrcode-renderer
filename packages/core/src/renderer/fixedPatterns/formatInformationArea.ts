3b05529f8506606ac2cb64079b293318878e30b8
Yurick <yurick.hauschild@gmail.com>
2021-01-23 17:29:35 +0100

    Add information area

--- packages/core/src/renderer/fixedPatterns/formatInformationArea.ts
+++ packages/core/src/renderer/fixedPatterns/formatInformationArea.ts
@@ -1,9 +1,53 @@
 import Pixel from '../pixel'
 import { Matrix } from '../types'
+import { FINDER_PATTERN_OUTER_SIZE } from './finder'
+
+const range = (start: number, size: number) =>
+  new Array(size).fill(start).map((num, position) => num + position)
+
+const getInformationAreaCoordinates = (qrCodeSize: number) => {
+  return [
+    {
+      x: 0,
+      y: FINDER_PATTERN_OUTER_SIZE + 1,
+      width: 1,
+      height: FINDER_PATTERN_OUTER_SIZE + 2,
+    },
+    {
+      x: FINDER_PATTERN_OUTER_SIZE + 1,
+      y: 0,
+      width: FINDER_PATTERN_OUTER_SIZE + 2,
+      height: 1,
+    },
+    {
+      x: FINDER_PATTERN_OUTER_SIZE + 1,
+      y: qrCodeSize - FINDER_PATTERN_OUTER_SIZE - 1,
+      width: FINDER_PATTERN_OUTER_SIZE + 1,
+      height: 1,
+    },
+    {
+      x: qrCodeSize - FINDER_PATTERN_OUTER_SIZE - 1,
+      y: FINDER_PATTERN_OUTER_SIZE + 1,
+      width: 1,
+      height: FINDER_PATTERN_OUTER_SIZE + 1,
+    },
+  ]
+}
 
 export const addFormatInformationAreaPattern = (
   version: number,
   matrix: Matrix
 ) => {
+  // AKA. the dark module
   matrix[4 * version + 9][8] = Pixel.BLACK
+
+  for (const coordinate of getInformationAreaCoordinates(matrix.length)) {
+    for (const x of range(coordinate.x, coordinate.height)) {
+      for (const y of range(coordinate.y, coordinate.width)) {
+        if (matrix[x][y] === Pixel.EMPTY) {
+          matrix[x][y] = Pixel.WHITE
+        }
+      }
+    }
+  }
 }
