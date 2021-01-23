e482d145732a7231e8fc3710f4d665f8f92f44b1
Yurick <yurick.hauschild@gmail.com>
2021-01-23 13:25:56 +0100

    Add simple integration testing

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,1 +1,6 @@
-export default () => true
+import encoder from './encoder'
+import { generateMatrix } from './renderer/matrix'
+
+export const createModuleDefinition = (input: string) => {
+  return generateMatrix(encoder(input))
+}
