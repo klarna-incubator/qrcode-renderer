9ba418cf6db25c85c2718e9856162aaf7d149f9e
Yurick <yurick.hauschild@gmail.com>
2021-01-23 15:33:54 +0100

    Add comment so it doesn't look like index is ready

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -2,5 +2,6 @@ import encoder from './encoder'
 import { generateMatrix } from './renderer/matrix'
 
 export const createModuleDefinition = (input: string) => {
+  // TODO: this is still incomplete, here we don't have any data about error correction, for example
   return generateMatrix(encoder(input))
 }
