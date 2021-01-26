650c8ba26e2a8199894c019052b49d310c6fcea3
Yurick <yurick.hauschild@gmail.com>
2021-01-26 19:40:07 +0100

    Expose optional options

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,10 +1,13 @@
-import encoder from './encoder'
+import encoder, { EncoderOptions } from './encoder'
 import { interleave } from './interleave'
 import { errorCorrection } from './errorCorrection'
 import { renderModules } from './renderer'
 
-export const createModuleDefinition = (input: string) => {
-  const { version, level, data } = encoder(input)
+export const createModuleDefinition = (
+  input: string,
+  options?: EncoderOptions
+) => {
+  const { version, level, data } = encoder(input, options)
   const message = interleave(errorCorrection(version, level, data))
 
   return renderModules({ version, level, data: message })
